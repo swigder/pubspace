702f079ab213c1cc28297fae85054faf60bcae65
suri <swigder@gmail.com>
2024-01-03 22:03:20 -0500

    Basic setup for converting public space type to category and indoor / outdoor.
    Next step is to color and filter based on this. However, need to revamp the filtering mechanism first.

--- set_up_data.py
+++ set_up_data.py
@@ -7,6 +7,36 @@ from collections import defaultdict
 
 DATASET_URL = 'https://data.cityofnewyork.us/resource/rvih-nhyn.csv'
 
+CATEGORIES = {
+    'Arcade': [],
+    'Covered': [],
+    'Enclosed': [
+        'Glass-Enclosed Urban Plaza Equivalent',
+    ],
+    'Lobby': [],
+    'Outdoor': [
+        'Landscaped Terrace',
+        'Landscaped Terraces',
+    ],
+    'Park': [],
+    'Passageway': [],
+    'Plaza': [
+        'Large Square',
+        'Small Square',
+    ],
+}
+
+TYPE_TO_CATEGORY = {i: k for k, v in CATEGORIES.items() for i in v}
+
+PROTECTION = {
+    'Outdoor': ['Outdoor', 'Park', 'Plaza'],
+    'Indoor': ['Enclosed', 'Lobby'],
+    'Covered': ['Arcade', 'Covered'],
+    'Other': ['Passageway'],
+}
+
+CATEGORY_TO_PROTECTION = {i: k for k, v in PROTECTION.items() for i in v}
+
 
 def get_data(force_cache_update=False):
     filename = 'data/pops.csv'
@@ -22,6 +52,17 @@ def get_data(force_cache_update=False):
     with open(filename, 'w') as outfile:
         outfile.write(content)
     return pandas.read_csv(filename)
+
+
+def get_categories_and_protections(public_space_types):
+    categories = []
+    for space_type in public_space_types:
+        if space_type in TYPE_TO_CATEGORY:
+            categories.append(TYPE_TO_CATEGORY[space_type])
+        else:
+            categories.extend([category for category in CATEGORIES if space_type in category])
+    protections = [CATEGORY_TO_PROTECTION[category] for category in categories]
+    return categories, protections
 
 
 def to_geojson(df):
@@ -46,6 +87,7 @@ def to_geojson(df):
         row_id = row['pops_number']
         amenities = to_list(row['amenities_required'])
         public_space_type = to_list(row['public_space_type'])
+        categories, protections = get_categories_and_protections(public_space_type)
 
         properties = {
             'id': row_id,
@@ -58,6 +100,8 @@ def to_geojson(df):
             'address': row['address_number'] + ' ' + row['street_name'].title(),
             'amenities': [emoji[a] + a for a in amenities],
             'public_space_type': public_space_type,
+            'categories': categories,
+            'protections': protections,
         }
         geojson_items.append(
             geojson.Feature(geometry=geojson.Point((row['longitude'], row['latitude'])),
@@ -68,6 +112,7 @@ def to_geojson(df):
         json.dump(geojson.FeatureCollection(features=geojson_items), outfile)
     with open('web/data/pops.json', 'w') as outfile:
         json.dump(detail_items, outfile)
+
 
 def main():
     data = get_data(force_cache_update=True)
